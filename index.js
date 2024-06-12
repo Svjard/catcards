4eb1c8a64a66a749ff8820a368df4a90a0f2d218
Marc Fisher <mcfisher83@gmail.com>
2024-06-11 19:22:39 -0500

    feat: Auto-populate the equipment cards from the JSON data

--- index.js
+++ index.js
@@ -1,6 +1,71 @@
 let equipmentData = [];
 
 /**
+ *  Renders the the specification details on the back of the equipment card.
+ */
+function renderSpecifications(el, specifications) {
+  el.replaceChildren();
+
+  specifications.forEach(s => {
+    const specTemp = document.getElementsByTagName('template')[2];
+    const spec = specTemp.content.cloneNode(true);
+    
+    spec.querySelector('.item').innerHTML = s.label;
+    spec.querySelector('.value').innerHTML = s.metric;
+
+    el.appendChild(spec);
+  });
+}
+
+/**
+ * Renders the equipment cards via the template and builds out the content within each card. 
+ */
+function renderCards(equipment) {
+  const container = document.querySelector('.cards-container');
+
+  container.replaceChildren();
+
+  equipment.forEach((model) => {
+    const cardTemp = document.getElementsByTagName('template')[0];
+    const card = cardTemp.content.cloneNode(true);
+
+    card.querySelector('.title').innerHTML = model.name;
+    card.querySelector('.machine-image').src = model.image;
+
+    const facts = card.querySelector('.facts');
+    model.keySpecs.forEach(spec => {
+      const factTemp = document.getElementsByTagName('template')[1];
+      const fact = factTemp.content.cloneNode(true);
+      
+      fact.querySelector('.fact-text').innerHTML += `${spec.label}: ${spec.metric}`;
+
+      facts.appendChild(fact);
+    });
+
+    const specCategoriesSet = new Set();
+    model.specifications.forEach(spec => specCategoriesSet.add(spec.category));
+
+    const specificationCategories = Array.from(specCategoriesSet).sort();
+    const sectionsDropdown = card.getElementById('sections');
+    specificationCategories.forEach(category => {
+      const opt = document.createElement('option');
+      opt.value = category;
+      opt.innerHTML = category;
+      sectionsDropdown.appendChild(opt);
+    });
+    sectionsDropdown.value = specificationCategories[0];
+
+    const specs = card.querySelector('.specifications');
+    renderSpecifications(
+      specs,
+      model.specifications.filter(s => s.category === specificationCategories[0])
+    );
+
+    container.appendChild(card);
+  });
+}
+
+/**
  * Entry point, read from the equipment data and then populate the initial
  * components on the screen with the information.
  */
@@ -32,6 +97,8 @@ fetch('data/equipment.json')
 
     // Set the <select>'s value to the first category by default
     categoryDropdown.value = keys[0];
+
+    renderCards(equipmentData.filter(d => d.category === keys[0]));
   })
   .catch(error => {
     /* eslint-disable-next-line no-console */
